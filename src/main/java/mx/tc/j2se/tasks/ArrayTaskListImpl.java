a4bab2a20d42450ba1a1ced551b3c59f0661aa98
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-06-28 16:44:17 -0500

    The ArrayTaskList interface, ArrayTaskListImpl class were created. The tests for each method were passed. The javadoc is ready.

---
+++ src/main/java/mx/tc/j2se/tasks/ArrayTaskListImpl.java
@@ -0,0 +1,132 @@
+package mx.tc.j2se.tasks;
+
+/**
+ * @version 1.10 28 June 2022
+ * @author Gonzalo Martínez
+ */
+
+public class ArrayTaskListImpl implements ArrayTaskList {
+
+    /**
+     * tasks is the initial variable used on the ArrayTaskListImpl class
+     */
+    private Task[] tasks = {};
+
+    /**
+     * Empty constructor of the ArrayTaskListImpl class
+     */
+    public ArrayTaskListImpl() {
+    }
+
+    /**
+     *add is a method that adds a specific task to the tasks list.
+     * The same task can be added to the list several times.
+     * @param task is the task to be added
+     */
+    @Override
+    public void add(Task task) {
+        Task[] tasksAdded = new Task[this.tasks.length + 1];
+
+        System.arraycopy(this.tasks, 0, tasksAdded, 0, this.tasks.length);
+        tasksAdded[tasksAdded.length - 1] = task;
+
+        this.tasks = tasksAdded;
+
+    }
+
+    /**
+     * It is a method that deletes a specific task from the tasks list.
+     * If the list contains the same task several times, any of them is removed.
+     * @param task is the task to be deleted
+     * @return a boolean value which indicates if the task is in the list.
+     */
+
+    @Override
+    public boolean remove(Task task) {
+        boolean isOnList = false;
+        int aux = 0;
+        int taskNumber = 0;
+
+        for (Task i : this.tasks) {
+            if (i == task) {
+                taskNumber++;
+                isOnList = true;
+            }
+        }
+
+        if (taskNumber == 1) {
+            Task[] tasksDeleted = new Task[this.tasks.length - 1];
+            for (Task j : this.tasks) {
+                if (j != task) {
+                    tasksDeleted[aux] = j;
+                    aux++;
+                }
+            }
+            this.tasks = tasksDeleted;
+        }
+        return isOnList;
+    }
+
+    /**
+     * It is a method that specifies the number of tasks that are in the list.
+     * @return the size of the list
+     */
+    @Override
+    public int size() {
+        return tasks.length;
+    }
+
+    /**
+     * It is a method that gets the task that is in a specific
+     * position in the list.
+     * @param index is the specified index of the required task to obtain
+     * @return the task in the specified index
+     */
+    @Override
+    public Task getTask(int index) {
+        return tasks[index];
+    }
+
+    /**
+     * It is a method that allows to obtain
+     * the active tasks to be executed in a certain time range.
+     * The from time must be less than to.
+     *
+     * @param from is the start of the interval
+     * @param to   is the end of the interval
+     * @return the list of tasks
+     */
+
+    @Override
+    public ArrayTaskList[] incoming(int from, int to) {
+        ArrayTaskList[] onRange = new ArrayTaskList[0];
+
+        for(Task i: this.tasks){
+            int i1 = onRange.length + 1;
+            if (i.isRepeated() && i.isActive()){
+                int add = i.getStartTime();
+                while(add < from) {
+                    add += i.getRepeatInterval();
+                }
+                if (add <= to && add <= i.getEndTime()) {
+
+                    ArrayTaskList[] tasksAdded = new ArrayTaskList[i1];
+                    System.arraycopy(onRange, 0, tasksAdded, 0, onRange.length);
+                    tasksAdded[tasksAdded.length-1] = (ArrayTaskList) i;
+
+                    onRange = tasksAdded;
+                }
+
+            } else if(!i.isRepeated() && i.isActive() && i.getTime() >= from && i.getTime() <= to) {
+                ArrayTaskList[] tasksAdded = new ArrayTaskList[i1];
+                System.arraycopy(onRange, 0, tasksAdded, 0, onRange.length);
+                tasksAdded[tasksAdded.length-1] = (ArrayTaskList) i;
+
+                onRange = tasksAdded;
+            }
+        }
+        return onRange;
+    }
+
+
+}
---
+++ src/test/java/mx/tc/j2se/tasks/ArrayTaskListImplTest.java
@@ -0,0 +1,126 @@
+package mx.tc.j2se.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTaskListImplTest {
+
+    @Test
+    void addTest() {
+        TaskImpl task = new TaskImpl("Test1", 100);
+        ArrayTaskList tasks = new ArrayTaskListImpl();
+        tasks.add(task);
+        assertEquals(1, tasks.size());
+
+        TaskImpl task1 = new TaskImpl("Test2", 100, 200, 50);
+        tasks.add(task1);
+        assertEquals(2, tasks.size());
+
+        TaskImpl task2 = new TaskImpl("Test3", 100, 200, 50);
+        tasks.add(task2);
+        assertEquals(3, tasks.size());
+
+        TaskImpl task3 = new TaskImpl("Test4", 100);
+        tasks.add(task2);
+        assertEquals(4, tasks.size());
+
+        tasks.add(task2);
+        assertEquals(5, tasks.size());
+
+        tasks.add(task);
+        assertEquals(6, tasks.size());
+    }
+
+    @Test
+    void removeTest() {
+        TaskImpl task = new TaskImpl("Test1", 100);
+        TaskImpl task2 = new TaskImpl("Test2", 100);
+        TaskImpl task3 = new TaskImpl("Test3", 100);
+        TaskImpl task4 = new TaskImpl("Test4", 100);
+        TaskImpl task5= new TaskImpl("Test4", 100, 200, 50);
+
+        ArrayTaskList tasks = new ArrayTaskListImpl();
+        tasks.add(task);
+        tasks.add(task);
+        tasks.add(task2);
+//        tasks.add(task2);
+        tasks.add(task3);
+        tasks.add(task4);
+//        tasks.add(task5);
+        tasks.add(task5);
+
+//        tasks.remove(task3);
+        tasks.remove(task4);
+        tasks.remove(task2);
+        tasks.remove(task);
+//        tasks.remove(task5);
+
+        assertEquals(4, tasks.size());
+        assertTrue(tasks.remove(task5));
+
+
+    }
+
+    @Test
+    void sizeTest() {
+        TaskImpl task = new TaskImpl("Test1", 100);
+        TaskImpl task2 = new TaskImpl("Test2", 100);
+        TaskImpl task3 = new TaskImpl("Test3", 100, 200, 50);
+        TaskImpl task4 = new TaskImpl("Test4", 100, 200, 50);
+        TaskImpl task5 = new TaskImpl("Test5", 100);
+
+        ArrayTaskList taskList = new ArrayTaskListImpl();
+        taskList.add(task);
+        taskList.add(task3);
+        taskList.add(task4);
+        taskList.add(task5);
+        taskList.remove(task5);
+        assertEquals(3, taskList.size());
+    }
+
+    @Test
+    void getTaskTest() {
+        TaskImpl task = new TaskImpl("Test1", 100);
+        TaskImpl task2 = new TaskImpl("Test2", 100);
+        TaskImpl task3 = new TaskImpl("Test3", 100, 200, 50);
+        TaskImpl task4 = new TaskImpl("Test4", 100, 200, 50);
+        TaskImpl task5 = new TaskImpl("Test5", 100);
+
+        ArrayTaskList taskList = new ArrayTaskListImpl();
+        taskList.add(task);
+        taskList.add(task3);
+        taskList.add(task4);
+        taskList.add(task5);
+
+        assertEquals(task5, taskList.getTask(3));
+    }
+
+    @Test
+    void incomingTest() {
+        TaskImpl task = new TaskImpl("Test1", 100);
+        task.setActive(true);
+
+        TaskImpl task2 = new TaskImpl("Test2", 100);
+
+        TaskImpl task3 = new TaskImpl("Test3", 100, 200, 50);
+        task3.setActive(true);
+
+        TaskImpl task4 = new TaskImpl("Test4", 100, 200, 50);
+        task4.setActive(true);
+
+        TaskImpl task5 = new TaskImpl("Test5", 100);
+//        task5.setActive(true);
+
+        ArrayTaskList taskList = new ArrayTaskListImpl();
+        taskList.add(task);
+        taskList.add(task2);
+        taskList.add(task3);
+        taskList.add(task4);
+        taskList.add(task5);
+
+//        taskList.incoming(50, 300);
+
+        assertEquals(0,taskList.incoming(250, 300).length);
+    }
+}
